c582b5ae54dc1b3ab78111b150749a6b79df93e2
Kyryh <kyrykiller07@gmail.com>
2024-01-19 16:54:23 +0100

    Added mùore input actions

--- FreeLookMonitorInput.cs
+++ FreeLookMonitorInput.cs
@@ -7,11 +7,11 @@ using UnityEngine.InputSystem;
 namespace FreeLookMonitor {
     internal class FreeLookMonitorInput : LcInputActions {
 
-        [InputAction("<Keyboard>/upArrow", Name = "Move up")]
-        public InputAction MoveUp { get; set;}
+        [InputAction("<Keyboard>/upArrow", Name = "Move forward")]
+        public InputAction MoveForward { get; set;}
 
-        [InputAction("<Keyboard>/downArrow", Name = "Move down")]
-        public InputAction MoveDown { get; set;}
+        [InputAction("<Keyboard>/downArrow", Name = "Move backward")]
+        public InputAction MoveBackward { get; set;}
 
         [InputAction("<Keyboard>/leftArrow", Name = "Move left")]
         public InputAction MoveLeft { get; set; }
@@ -19,16 +19,23 @@ namespace FreeLookMonitor {
         [InputAction("<Keyboard>/rightArrow", Name = "Move right")]
         public InputAction MoveRight { get; set;}
 
+        [InputAction("<Keyboard>/pageUp", Name = "Move up")]
+        public InputAction MoveUp { get; set; }
+
+        [InputAction("<Keyboard>/pageDown", Name = "Move down")]
+        public InputAction MoveDown { get; set; }
+
         [InputAction("<Keyboard>/insert", Name = "Reset position")]
         public InputAction ResetPosition { get; set; }
 
-        [InputAction("<Keyboard>/pageDown", Name = "Zoom in")]
+        [InputAction("<Keyboard>/plus", Name = "Zoom in")]
         public InputAction ZoomIn { get; set; }
 
-        [InputAction("<Keyboard>/pageUp", Name = "Zoom out")]
+        [InputAction("<Keyboard>/minus", Name = "Zoom out")]
         public InputAction ZoomOut { get; set; }
 
-        
+
+
     }
 
     
